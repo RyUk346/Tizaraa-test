712455ddc1e6c2956d5cd10be7a8bf8a694e4301
RyUk346 <mahmudur346@gmail.com>
2025-02-04 21:10:58 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,13 @@ import NavBar from "./components/navbar";
 import Home from "./components/home";
 import Product from "./components/product";
 import Offer from "./components/offer";
-import Summer from "./components/summer";
+
 import Footer from "./components/footer";
 import { CartProvider } from "./components/cart";
 
 export default function App() {
   return (
     <CartProvider>
-      {" "}
-      {/* Wrap the entire app with CartProvider */}
       <div className="font-manrope">
         <div className="sticky top-0 z-50">
           <NavBar />
@@ -21,9 +19,7 @@ export default function App() {
         </div>
         <Product />
         <Offer />
-        <div id="deals">
-          <Summer />
-        </div>
+
         <Footer />
       </div>
     </CartProvider>
